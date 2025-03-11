1a9c44cad41676b8b0d63181a2f11d72bae7bf54
Smart Samuel <smartsamuel017@gmail.com>
2025-03-11 22:23:21 +0100

    Adjustments made

--- admin/login.php
+++ admin/login.php
@@ -25,7 +25,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
+          <input type="password" class="form-control" name="password" value="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
